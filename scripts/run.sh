9de120b22b6501b22b8598ebf2c3f9a55050fbbf
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-12-16 17:36:09 -0500

    updated scrips and templates, removed str(..), added conditional deployment strategy based on value of env variable

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,6 +8,6 @@ LAUNCH_DIR=$(pwd); SCRIPT_DIR=$(dirname $0); cd $SCRIPT_DIR; SCRIPT_DIR=$(pwd); 
 
 cd $SCRIPT_PARENT_DIR
 
-ytt -f config/config.yaml -f env/values-dev.yaml -f base/
+ytt -f config/config.yaml -f env/values-${ENV}.yaml -f base/
 
 cd $LAUNCH_DIR
