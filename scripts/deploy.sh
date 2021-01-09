55d91847414140cbfabd8e77f67800f689d66178
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-01-09 11:52:36 -0500

    cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,9 @@
 # set -x
 
 LAUNCH_DIR=$(pwd); SCRIPT_DIR=$(dirname $0); cd $SCRIPT_DIR; SCRIPT_DIR=$(pwd); cd ..; SCRIPT_PARENT_DIR=$(pwd)
-# echo LAUNCH_DIR=$LAUNCH_DIR; echo SCRIPT_DIR=$SCRIPT_DIR; echo SCRIPT_PARENT_DIR=$SCRIPT_PARENT_DIR
 . $SCRIPT_DIR/set-env.sh
 
 cd $SCRIPT_PARENT_DIR
-
-# ytt -f config/config.yaml -f env/values-${ENV}.yaml -f base/
 
 kubectl create ns ${ENV}-spring-petclinic
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 # set -x
 
 LAUNCH_DIR=$(pwd); SCRIPT_DIR=$(dirname $0); cd $SCRIPT_DIR; SCRIPT_DIR=$(pwd); cd ..; SCRIPT_PARENT_DIR=$(pwd)
-# echo LAUNCH_DIR=$LAUNCH_DIR; echo SCRIPT_DIR=$SCRIPT_DIR; echo SCRIPT_PARENT_DIR=$SCRIPT_PARENT_DIR
+
 . $SCRIPT_DIR/set-env.sh
 
 cd $SCRIPT_PARENT_DIR
